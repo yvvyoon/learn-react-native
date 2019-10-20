ad7d1d237bd4a51bdee2d9d1b198cad2a5bae8e8
yvvyoon <yoon4480@naver.com>
2019-10-20 16:40:22 +0900

    Add UI, Geolocation data

--- nice-weather/App.js
+++ nice-weather/App.js
@@ -1,38 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert } from 'react-native';
+import Loading from './loading';
+import * as Location from 'expo-location';
+import axios from 'axios';
+import { } from 'dotenv/config';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.blueView} />
-      <View style={styles.whiteView} />
-      <View style={styles.redView} />
-    </View>
-  );
-}
+const API_KEY = process.env.API_KEY;
 
-const styles = StyleSheet.create({
-  container: {
-    // 모든 공간이 사용가능하다는 의미
-    flex: 1,
-    // flexDirection은 자식 컴포넌트들의 가로 세로 배치를 결정하는데,
-    // default는 column이고 세로로 배치함.
-    // row로 설정하면 가로로 배치함.
-    flexDirection: 'row'
-  },
-  blueView: {
-    // 자식 컴포넌트들의 flex는 비율 개념인데,
-    // 1과 3이면 1/4, 3/4로 레이아웃이 설정됨
-    // 여기서는 blue가 화면의 3/4를 차지
-    flex: 1,
-    backgroundColor: 'blue',
-  },
-  whiteView: {
-    flex: 1,
-    backgroundColor: 'white',
-  },
-  redView: {
-    flex: 1,
-    backgroundColor: 'red',
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  getWeather = async (latitude, longitude) => {
+    // axios 모듈 GET 방식으로 API를 통해 얻은 데이터 fetch
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
   }
-});
+
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      // const response = await Location.requestPermissionsAsync();
+      // permission에 대해서는 undefined
+      // console.log(response);
+
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      // console.log(coords.latitude, coords.longitude);
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: true });
+
+      // 에러 던지기 
+      // throw Error();
+    }
+    catch (error) {
+      Alert.alert('위치를 알 수 없습니다.', '힝');
+    }
+  }
+
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+
+    // isLoading이 true이면 <Loading />, false이면 null 리턴
+    return isLoading ? <Loading /> : null;
+  }
+}
