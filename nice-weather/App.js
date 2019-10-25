e88d93edecfe0e899289810bc8698e00379c61fd
yvvyoon <yoon4480@naver.com>
2019-10-25 20:19:57 +0900

    Add Weather.js

--- nice-weather/App.js
+++ nice-weather/App.js
@@ -1,55 +1,68 @@
 import React from 'react';
 import { Alert } from 'react-native';
-import Loading from './loading';
+import Loading from './Loading';
+import Weather from './Weather';
 import * as Location from 'expo-location';
 import axios from 'axios';
-import { } from 'dotenv/config';
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = '00d697c5fe1c098a9832ae421bc90989';
 
 export default class extends React.Component {
-  state = {
-    isLoading: true
-  };
+	state = {
+		isLoading: true,
+	};
 
-  getWeather = async (latitude, longitude) => {
-    // axios 모듈 GET 방식으로 API를 통해 얻은 데이터 fetch
-    const { data } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
-    );
-    console.log(data);
-  }
+	getWeather = async (latitude, longitude) => {
+		// axios 모듈 GET 방식으로 API를 통해 얻은 데이터 fetch
+		const {
+			data: {
+        main: { temp },
+        weather
+			},
+		} = await axios.get(
+			`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`,
+		);
 
-  getLocation = async () => {
-    try {
-      await Location.requestPermissionsAsync();
-      // const response = await Location.requestPermissionsAsync();
-      // permission에 대해서는 undefined
-      // console.log(response);
+		this.setState({
+			isLoading: false,
+			condition: weather[0].main,
+			// ES6 문법으로
+			temp,
+		});
+	};
 
-      const {
-        coords: { latitude, longitude }
-      } = await Location.getCurrentPositionAsync();
-      // console.log(coords.latitude, coords.longitude);
-      this.getWeather(latitude, longitude);
-      this.setState({ isLoading: true });
+	getLocation = async () => {
+		try {
+			await Location.requestPermissionsAsync();
+			// const response = await Location.requestPermissionsAsync();
+			// permission에 대해서는 undefined
+			// console.log(response);
 
-      // 에러 던지기 
-      // throw Error();
-    }
-    catch (error) {
-      Alert.alert('위치를 알 수 없습니다.', '힝');
-    }
-  }
+			const {
+				coords: { latitude, longitude },
+			} = await Location.getCurrentPositionAsync();
+			// console.log(coords.latitude, coords.longitude);
+			this.getWeather(latitude, longitude);
 
-  componentDidMount() {
-    this.getLocation();
-  }
+			// 에러 던지기
+			// throw Error();
+		} catch (error) {
+			Alert.alert('위치를 알 수 없습니다.', '힝');
+		}
+	};
 
-  render() {
-    const { isLoading } = this.state;
+	componentDidMount() {
+		this.getLocation();
+	}
 
-    // isLoading이 true이면 <Loading />, false이면 null 리턴
-    return isLoading ? <Loading /> : null;
-  }
+	render() {
+		const { isLoading, temp, condition } = this.state;
+
+		// isLoading이 true이면 <Loading />, false이면 null 리턴
+		return isLoading ? (
+			<Loading />
+		) : (
+			<Weather temp={Math.round(temp)} condition={condition} />
+		);
+	}
 }
--- nice-weather/loading.js
+++ nice-weather/loading.js
@@ -2,21 +2,23 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Loading() {
-    return <View style={styles.container}>
-        <Text style={styles.text}>Getting the nice weather</Text>
-    </View>
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Getting the nice weather</Text>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        paddingHorizontal: 30,
-        paddingVertical: 100,
-        backgroundColor: '#3EB489'
-    },
-    text: {
-        color: '#2C2C2C',
-        fontSize: 30
-    }
+	container: {
+		flex: 1,
+		justifyContent: 'flex-end',
+		paddingHorizontal: 30,
+		paddingVertical: 100,
+		backgroundColor: '#3EB489',
+	},
+	text: {
+		color: '#2C2C2C',
+		fontSize: 30,
+	},
 });
